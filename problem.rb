8a15aae72626269aeaf3060c42fd05cf712d52c4
Nikhat Bhayani <nikhataly@gmail.com>
2015-06-16 14:54:45 -0400

    First commit

---
+++ problem.rb
@@ -0,0 +1,24 @@
+# Write a program to get the answer if you divide two numbers:
+
+# Ex:
+# 10/5 = 2
+# -35/7 = -5
+# 6/2 = 3
+
+# -You cant use / * %
+# -Try to think of edge cases as well
+
+def return_quotient(a, b)
+
+  case positive_or_negative
+  when a == 0 && b == 0
+    "0"
+  else
+    "hi"
+
+  end
+
+end
+
+# assert_equal "0".to_i, return_quotient(0, 0)
+# return_quotient
